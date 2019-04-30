339d8a72d4cd65595432b238c0c9620deb530a76
Tom McLaughlin <pyro777@gmail.com>
2019-04-30 03:30:23 -0700

    Add tests of sha256 hashes

--- pkgs/build-support/docker/tests/util.py
+++ pkgs/build-support/docker/tests/util.py
@@ -5,7 +5,7 @@ import json
 import os
 import subprocess
 
-def validate_image(image_dir, num_layers=None, num_symlink_layers=None, check_sha256=False):
+def validate_image(image_dir, num_layers=None, num_symlink_layers=None):
     """Perform sanity checks to validate that a Docker image folder is well-formed"""
     assert os.path.isdir(image_dir)
 
@@ -16,22 +16,30 @@ def validate_image(image_dir, num_layers=None, num_symlink_layers=None, check_sh
     with open(manifest_path) as f:
         manifest = json.loads(f.read())
 
-    layer_dirs = []
+    layers = []
+    config_path = None
     for section in manifest:
         for layer in section["Layers"]:
             assert os.path.isfile(image_dir.join(layer))
-            layer_dirs.append(image_dir.join(layer))
+            layers.append(image_dir.join(layer))
+
+        assert section["Config"]
+        config_path = section["Config"]
 
         assert os.path.isfile(image_dir.join(section["Config"]))
 
     if num_layers is not None:
-        assert len(layer_dirs) == num_layers
+        assert len(layers) == num_layers
 
     if num_symlink_layers is not None:
-        assert len([x for x in layer_dirs if os.path.islink(x)]) == num_symlink_layers
+        assert len([x for x in layers if os.path.islink(x)]) == num_symlink_layers
 
-    if check_sha256:
-        raise Exception("TODO: implement checking of the layers using sha256sum")
+    # Check layer sha256 hashes
+    with open(image_dir.join(config_path)) as f:
+        config = json.loads(f.read())
+    for (layer, desired_hash) in zip(layers, config["rootfs"]["diff_ids"]):
+        found_hash = subprocess.check_output(["sha256sum", layer]).decode().split(" ")[0]
+        assert "sha256:" + found_hash == desired_hash
 
 @contextmanager
 def docker_load(full_image_name, tarball):

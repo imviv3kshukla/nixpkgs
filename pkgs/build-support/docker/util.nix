14a11dd364528b5bf2350a6665548c81bfd2c38c
Tom McLaughlin <pyro777@gmail.com>
2019-12-09 02:39:20 -0800

    Fix tarsum in docker

--- pkgs/build-support/docker/util.nix
+++ pkgs/build-support/docker/util.nix
@@ -12,13 +12,14 @@ rec {
   # We need to sum layer.tar, not a directory, hence tarsum instead of nix-hash.
   # And we cannot untar it, because then we cannot preserve permissions ecc.
   tarsum = runCommand "tarsum" {
-    buildInputs = [ go ];
+    nativeBuildInputs = [ go ];
   } ''
     mkdir tarsum
     cd tarsum
 
     cp ${./tarsum.go} tarsum.go
     export GOPATH=$(pwd)
+    export GOCACHE="$TMPDIR/go-cache"
     mkdir -p src/github.com/docker/docker/pkg
     ln -sT ${docker.src}/components/engine/pkg/tarsum src/github.com/docker/docker/pkg/tarsum
     go build

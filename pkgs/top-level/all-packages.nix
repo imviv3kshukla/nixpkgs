12c5aa41ceafc08fcb03536218a1cad105fd5591
Tom McLaughlin <tom@codedown.io>
2020-04-23 22:18:00 -0700

    python-language-server: init at 2020-04-24

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10412,6 +10412,10 @@ in
 
   mdl = callPackage ../development/tools/misc/mdl { };
 
+  python-language-server = callPackage ../development/dotnet-modules/python-language-server {
+    inherit (dotnetPackages) Nuget;
+  };
+
   minify = callPackage ../development/web/minify { };
 
   minizinc = callPackage ../development/tools/minizinc { };

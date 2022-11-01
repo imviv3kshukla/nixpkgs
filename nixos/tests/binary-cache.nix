97065a5e91b7abca41333eb05de077c1ce6cf088
Tom McLaughlin <tom@codedown.io>
2022-11-01 14:11:35 -0700

    Improve VM tests

--- nixos/tests/binary-cache.nix
+++ nixos/tests/binary-cache.nix
@@ -9,8 +9,8 @@ with lib;
   nodes.machine =
     { pkgs, ... }: {
       imports = [ ../modules/installer/cd-dvd/channel.nix ];
-      environment.systemPackages = with pkgs; [hello python3];
-      system.extraDependencies = with pkgs; [hello.inputDerivation];
+      environment.systemPackages = with pkgs; [python3];
+      system.extraDependencies = with pkgs; [hello hello.inputDerivation];
       nix.extraOptions = ''
         experimental-features = nix-command
       '';
@@ -25,7 +25,16 @@ with lib;
     assert ("nix-cache-info" in cache_files)
     assert ("nar" in cache_files)
 
+    # Cache should contain a .narinfo referring to "hello"
+    machine.succeed("grep 'StorePath: /nix/store/[[:alnum:]]*-hello-' /tmp/cache/*.narinfo")
+
     # Nix store ping should work
     machine.succeed("nix store ping --store file:///tmp/cache")
+
+    # Should be able to build hello using the cache
+    logs = machine.succeed("nix-build -A hello '<nixpkgs>' --option require-sigs false --option trusted-substituters file:///tmp/cache 2>&1")
+    import re
+    match = re.match(r"^/nix/store/[a-z0-9]*-hello-.*$", str(logs))
+    if not match: raise Exception("Output didn't contain reference to built hello")
   '';
 })

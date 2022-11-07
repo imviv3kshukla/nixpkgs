583932094ef78893c21f96039ad05bc626f0ad8f
Tom McLaughlin <tom@codedown.io>
2022-11-07 08:53:39 -0800

    More test progress

--- nixos/tests/binary-cache.nix
+++ nixos/tests/binary-cache.nix
@@ -25,16 +25,27 @@ with lib;
     assert ("nix-cache-info" in cache_files)
     assert ("nar" in cache_files)
 
-    # Cache should contain a .narinfo referring to "hello"
-    machine.succeed("grep 'StorePath: /nix/store/[[:alnum:]]*-hello-' /tmp/cache/*.narinfo")
-
     # Nix store ping should work
     machine.succeed("nix store ping --store file:///tmp/cache")
 
+    # Cache should contain a .narinfo referring to "hello"
+    grepLogs = machine.succeed("grep -l 'StorePath: /nix/store/[[:alnum:]]*-hello-.*' /tmp/cache/*.narinfo")
+
+    # Get the store path referenced by the .narinfo and verify it's not in the store yet
+    narInfoFile = grepLogs.strip()
+    narInfoContents = machine.succeed("cat " + narInfoFile)
+    import re
+    match = re.match(r"^StorePath: (/nix/store/[a-z0-9]*-hello-.*)$", narInfoContents, re.MULTILINE)
+    if not match: raise Exception("Couldn't find hello store path in cache")
+    storePath = match[1]
+    machine.fail("which " + storePath)
+
     # Should be able to build hello using the cache
     logs = machine.succeed("nix-build -A hello '<nixpkgs>' --option require-sigs false --option trusted-substituters file:///tmp/cache 2>&1")
-    import re
     match = re.match(r"^/nix/store/[a-z0-9]*-hello-.*$", str(logs))
     if not match: raise Exception("Output didn't contain reference to built hello")
+
+    # Store path should exist in the store now
+    machine.succeed("which " + storePath)
   '';
 })

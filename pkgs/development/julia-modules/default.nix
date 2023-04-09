7c3c9daea40831f852a80f426f023b761668a2e3
Tom McLaughlin <tom@codedown.io>
2023-04-08 17:12:06 -0700

    Add comments, refactor, prepare to deal with artifacts

--- pkgs/development/julia-modules/default.nix
+++ pkgs/development/julia-modules/default.nix
@@ -16,17 +16,20 @@
 }:
 
 let
-  augmentedRegistry = fetchFromGitHub {
-    owner = "CodeDownIO";
-    repo = "General";
-    rev = "6ff5a36621f25b2e0db69e917164d52894e016d6";
-    sha256 = "0y3130h97iyx947j0m0v6qfnklwmfg9jnllwa30kbblxm4fdh9wd";
-  };
+  # Special registry which is equal to JuliaRegistries/General, but every Versions.toml
+  # entry is augmented with a Nix sha256 hash
+  augmentedRegistry = callPackage ./registry.nix {};
 
+  # Invoke Julia resolution logic to determine the full dependency closure
   closureYaml = callPackage ./package-closure.nix {
     inherit julia augmentedRegistry packageNames;
   };
 
+  # Generate a Nix file consisting of a map from dependency UUID --> fetchgit call:
+  # {
+  #   "77ba4419-2d1f-58cd-9bb1-8ffee604a2e3" = fetchgit {...};
+  #   ...
+  # }
   dependencies = runCommand "julia-sources.nix" { buildInputs = [(python3.withPackages (ps: with ps; [toml pyyaml]))]; } ''
     export OUT="$out"
     python ${./sources_nix.py} \
@@ -35,6 +38,17 @@ let
       "$out"
   '';
 
+  # Import the Nix file from the previous step (IFD) and turn each dependency repo into
+  # a dummy Git repository, as Julia expects. Format the results as a YAML map from
+  # dependency UUID -> Nix store location:
+  # {
+  #   "77ba4419-2d1f-58cd-9bb1-8ffee604a2e3":"/nix/store/...-NaNMath.jl-0877504",
+  #   ...
+  # }
+  dependenciesYaml = writeTextFile {
+    name = "julia-dependencies.yml";
+    text = lib.generators.toYAML {} (lib.mapAttrs repoify (import dependencies { inherit fetchgit; }));
+  };
   repoify = name: src:
     runCommand ''julia-${name}-repoified'' {buildInputs = [git];} ''
       mkdir -p $out
@@ -46,11 +60,9 @@ let
       git config user.name "julia2nix"
       git commit -m "Dummy commit"
     '';
-  dependenciesYaml = writeTextFile {
-    name = "julia-dependencies.yml";
-    text = lib.generators.toYAML {} (lib.mapAttrs repoify (import dependencies { inherit fetchgit; }));
-  };
 
+  # Given the augmented registry, closure info yaml, and dependency path yaml, construct a complete
+  # Julia registry containing all the necessary packages
   minimalRegistry = runCommand "minimal-julia-registry" { buildInputs = [(python3.withPackages (ps: with ps; [toml pyyaml]))]; } ''
     python ${./minimal_registry.py} \
       "${augmentedRegistry}" \
@@ -59,6 +71,10 @@ let
       "$out"
   '';
 
+  # Next, deal with artifacts. Scan each artifacts file individually.
+
+  # Build a Julia project and depot. The project contains Project.toml/Manifest.toml, while the
+  # depot contains package build products (including the precompiled libraries, if precompile=true)
   projectAndDepot = callPackage ./depot.nix {
     inherit extraLibs packageNames precompile;
     registry = minimalRegistry;
---
+++ pkgs/development/julia-modules/extract_artifacts.jl
@@ -0,0 +1,76 @@
+
+using Pkg, Pkg.Artifacts, Pkg.BinaryPlatforms, Pkg.PlatformEngines
+
+dir = ARGS[1]
+
+artifacts_toml = find_artifacts_toml(dir)
+
+if artifacts_toml == nothing
+    print("{}")
+    exit()
+end
+
+artifact_dict = Pkg.Artifacts.load_artifacts_toml(artifacts_toml)
+
+platform = platform_key_abi()
+
+# Check for a select_artifacts.jl.
+# See https://pkgdocs.julialang.org/v1/artifacts/#Extending-Platform-Selection
+selectArtifactsJl = joinpath([dir, ".pkg", "select_artifacts.jl"])
+if isfile(selectArtifactsJl)
+  include(selectArtifactsJl)
+  extraMetas = augment_platform!(platform_key_abi()) # Unique copy of platform since it may alter it
+  # TODO: add these extra artifacts to artifact_dict so they get processed below
+end
+
+# It's possible for the artifacts to have multiple entries with the same sha1,
+# so group by that first
+sha1_to_meta = Dict{String, Vector{Tuple{String, Any}}}()
+for (artifact_name, items) in artifact_dict
+    if !isa(items, Vector)
+        items = [items];
+    end
+
+    # Get all possible artifacts that match our current os/arch.
+    # If they don't specify either key, consider it a match just to be safe.
+    for meta in items
+        if !(
+            !haskey(meta, "os") || meta["os"] == platform["os"]
+            &&
+            !haskey(meta, "arch") || meta["arch"] == platform["arch"]
+            )
+            continue
+        end
+
+        sha1 = meta["git-tree-sha1"]
+        if !haskey(sha1_to_meta, sha1)
+            sha1_to_meta[sha1] = []
+        end
+        push!(sha1_to_meta[sha1], (artifact_name, meta))
+    end
+end
+
+# Print a Nix attrset where the keys are sha1 hashes and the values
+# are lists of "artifact infos"
+print("{")
+for (sha1, metas) in sha1_to_meta
+    print("""\n  "$sha1" = [""")
+
+    for (index, (artifact_name, meta)) in enumerate(metas)
+        for download in meta["download"]
+            url = download["url"]
+            sha256 = download["sha256"]
+            if index > 1
+                print(" ")
+            end
+            print("{")
+            print("\n    name = \"$artifact_name\";")
+            print("\n    url = \"$url\";")
+            print("\n    sha256 = \"$sha256\";")
+            print("\n  }")
+        end
+    end
+
+    print("];")
+end
+print("\n}")
